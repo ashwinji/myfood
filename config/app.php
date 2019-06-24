59daaa52b58485acbe407c965331022b57b65983
ashwinjuwekar2018 <ajjuwekar@gmail.com>
2019-06-24 13:41:48 +0530

    new branch created;

--- config/app.php
+++ config/app.php
@@ -237,7 +237,6 @@ return [
         'Input' => Illuminate\Support\Facades\Input::class,
         'Excel' => Maatwebsite\Excel\Facades\Excel::class,
         'Image' => Intervention\Image\Facades\Image::class,
-
         'DataTables' => Yajra\DataTables\Facades\DataTables::class,
 
     ],
